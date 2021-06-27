38398ea96eef5eb55cc171881b58dd38a3fe2f18
loncat <me@lcat.dev>
2021-06-28 02:27:53 +0800

    Added log and simple user management features

---
+++ app/admin.py
@@ -0,0 +1,99 @@
+import re
+from flask import (Blueprint, abort, flash, redirect, render_template, request,
+                   session, url_for)
+
+from .models import LogPost, User, db
+from .views import requires_auth
+
+bp = Blueprint('admin', __name__, url_prefix='/administrator')
+
+
+@bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if session.get('authed'):
+        return redirect(url_for('root'))
+    elif request.method == 'POST':
+        if User.authenticate(request.form.get('password', '')):
+            session['authed'] = True
+            flash('Authentication successful')
+            return redirect(url_for('root'))
+        elif request.form.get('password') == 'password':
+            flash('No, not this time.')
+        else:
+            flash('Invalid password')
+    return render_template('admin/sudo.html')
+
+@bp.route('/change-password', methods=['GET', 'POST'])
+@requires_auth
+def change_password():
+    if request.method == 'POST':
+        new_password = request.form.get('new_password')
+        confirm_new_password = request.form.get('confirm_new_password')
+
+        if not (new_password and confirm_new_password):
+            abort(403)
+        elif new_password != confirm_new_password:
+            flash('Password unchanged')
+        else:
+            User.change_password(new_password)
+            flash('Password changed')
+            return logout()
+    return render_template('admin/change_password.html')
+
+@bp.route('/logout')
+@requires_auth
+def logout():
+    session.pop('authed')
+    return redirect(url_for('root'))
+
+
+@bp.route('/write-log', methods=['GET', 'POST'])
+@requires_auth
+def write_log():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        content = request.form.get('content')
+        is_markdown = request.form.get('is_md') is not None
+
+        if not (title and content):
+            abort(403)
+
+        log = LogPost(title, content, is_markdown)
+        db.session.add(log)
+        db.session.commit()
+
+        flash('Log post added successfully.')
+        return redirect(url_for('root'))
+
+    return render_template('admin/write_log.html')
+
+
+@bp.route('/edit-log/<log_post_id>', methods=['GET', 'POST'])
+@requires_auth
+def edit_log(log_post_id):
+    log_post = LogPost.query.filter_by(id=log_post_id).first_or_404()
+    if request.method == 'POST':
+        title = request.form.get('title')
+        content = request.form.get('content')
+        is_markdown = request.form.get('is_md') is not None
+
+        if not (title and content):
+            abort(403)
+
+        log_post.title = title
+        log_post.content = content
+        log_post.is_markdown = is_markdown
+        db.session.commit()
+
+        flash(f'Updated log post {log_post.id}.')
+
+    return render_template('admin/edit_log.html', log_post=log_post)
+
+
+@bp.route('/delete/<log_post_id>')
+def delete_log(log_post_id):
+    log_post = LogPost.query.filter_by(id=log_post_id).first_or_404()
+    db.session.delete(log_post)
+    db.session.commit()
+    flash(f'Deleted log post {log_post.id}')
+    return redirect(url_for('root'))
--- app/views.py
+++ app/views.py
@@ -1,12 +1,46 @@
-from flask import render_template
+from functools import wraps
+
+import marko
+from flask import redirect, render_template, request, session, url_for
 
 from . import app
+from .models import LogPost
+
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if 'authed' not in session:
+            return redirect(url_for('admin.login'))
+        return f(*args, **kwargs)
+
+    return decorated
 
 
 @app.route('/')
 def root():
-    return render_template('home.html')
+    log_posts = LogPost.query.order_by(LogPost.creation_ts.desc()).all()
+    return render_template('home.html', log_posts=log_posts)
 
-@app.route('/sudo')
-def sudo():
-    return render_template('sudo.html')
+
+@app.route('/log/<log_post_id>')
+@app.route('/log/<log_post_id>/<view_type>')
+def view_log(log_post_id, view_type=None):
+    log_post = LogPost.query.filter_by(id=log_post_id).first_or_404()
+
+    if view_type == 'download':
+        return log_post.content, {
+            'Content-Type': 'application/octet-stream',
+            'Content-Disposition': f'attachment;filename={log_post.id}'
+        }
+    elif view_type == 'raw':
+        return log_post.content, {'Content-Type': 'text/plain'}
+
+    processed_content = log_post.content
+
+    if log_post.is_markdown:
+        processed_content = marko.convert(log_post.content)
+
+    return render_template('view_log.html',
+                           log_post=log_post,
+                           processed_content=processed_content)
