8c6caf16216c7f02330a1a09a298f39367e6e9e7
loncat <me@lcat.dev>
2021-06-28 02:27:53 +0800

    Added log and simple user management features

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,15 @@
+import os
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-from .views import *  # noqa
+app.config.from_pyfile(os.path.join(app.root_path, '..', 'config.py'))
 
+db = SQLAlchemy(app)
+
+from .views import *  # noqa
+from .admin import bp as admin_bp  # noqa
+
+app.register_blueprint(admin_bp)
--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
 import datetime
 import uuid
+from hashlib import sha512
 
-from . import db
+from . import app, db
 
 
 class LogPost(db.Model):
@@ -11,13 +12,38 @@ class LogPost(db.Model):
                             default=datetime.datetime.now())
     title = db.Column(db.Text, nullable=False)
     content = db.Column(db.Text, nullable=True)
-    use_mdparser = db.Column(db.Boolean, nullable=True)
+    is_markdown = db.Column(db.Boolean, nullable=True)
 
-    def __init__(self, title, content, use_mdparser=True):
+    def __init__(self, title, content, is_markdown=True):
         self.id = str(uuid.uuid4())
         self.title = title
         self.content = content
-        self.use_mdparser = use_mdparser
+        self.is_markdown = is_markdown
+
+
+class User:
+    @classmethod
+    def authenticate(cls, password):
+        cred_file = app.config['CRED_FILE']
+        try:
+            f = open(cred_file)
+            f.close()
+        except FileNotFoundError:
+            cls.change_password('password')
+
+        with open(cred_file) as f:
+            password_r = f.read()
+            password_i = sha512(password.encode()).hexdigest()
+            if password_r == password_i:
+                return True
+        return False
+
+    @staticmethod
+    def change_password(new_password):
+        cred_file = app.config['CRED_FILE']
+        with open(cred_file, 'w') as f:
+            f.write(sha512(new_password.encode()).hexdigest())
+        return True
 
 
 db.create_all()
