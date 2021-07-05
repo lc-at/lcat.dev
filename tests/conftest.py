2f5b4178d8bf0cda21608fe48384e55a94878628
loncat <me@lcat.dev>
2021-07-05 11:18:07 +0800

    Added option for test_config

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,15 @@
 import pytest
-from app import app as flask_app
+from app import create_app
 from app.models import LogPost
 
 
 @pytest.fixture(scope='module')
 def client():
+    flask_app = create_app()
     with flask_app.test_client() as testing_client:
         with flask_app.app_context():
             yield testing_client
+
 
 
 @pytest.fixture(scope='module')
