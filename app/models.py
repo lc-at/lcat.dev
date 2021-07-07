d8f9d37134ffed8fdf1e09cb33146532d5238b22
loncat <me@lcat.dev>
2021-07-07 13:27:40 +0800

    Added invalid hash and empty `new_password` argument in `User.change_password`

--- app/models.py
+++ app/models.py
@@ -35,20 +35,24 @@ class User:
     def authenticate(cls, password):
         cred_file = current_app.config['CRED_FILE']
         try:
-            f = open(cred_file)
-            f.close()
-        except FileNotFoundError:
-            cls.change_password('password')
-
-        with open(cred_file) as f:
-            password_r = f.read()
-            password_i = sha512(password.encode()).hexdigest()
-            if password_r == password_i:
+            with open(cred_file) as f:
+                password_r = f.read()
+                if len(password_r) != 128:  # sha512 hash length is 128
+                    raise ValueError
+                password_i = sha512(password.encode()).hexdigest()
+                if password_r == password_i:
+                    return True
+        except (FileNotFoundError, ValueError):
+            default_password = 'password'
+            cls.change_password(default_password)
+            if password == default_password:
                 return True
         return False
 
     @staticmethod
     def change_password(new_password):
+        if not new_password:
+            return False
         cred_file = current_app.config['CRED_FILE']
         with open(cred_file, 'w') as f:
             f.write(sha512(new_password.encode()).hexdigest())
