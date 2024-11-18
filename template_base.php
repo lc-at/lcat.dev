dddb91b61be763f2518d6cd832219ec4ef30963a
Faiz Jazadi <me@lcat.dev>
2024-11-18 13:18:35 +0700

    Revert "Change font to Iosevka"
    This reverts commit 8412514a326090acc770c6db2204d40c1052d054.

--- template_base.php
+++ template_base.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>lcat &mdash; <?= $title ?></title>
 
+    <link href="https://fonts.cdnfonts.com/css/fantasque-sans-mono" rel="stylesheet">
     <link href="style.css" rel="stylesheet">
 </head>
 
